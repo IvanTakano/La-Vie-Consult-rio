b85ef58ab27f2f64e352676137f133789657905c
Ivan Yura Takano <ivanyurat@gmail.com>
2022-06-09 19:47:18 -0300

    sequelizar-cli

--- src/infrastructure/database/config/config.js
+++ src/infrastructure/database/config/config.js
@@ -2,18 +2,18 @@ require('dotenv').config()
 
 module.exports = {
   "development": {
-    "username": "root",
-    "password": "cawetr11",
-    "database": "la-vie",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.BD_DIALECT,
+    "dialect": process.env.DB_HOST,
   },
   "test": {
-    "username": "root",
-    "password": "cawetr11",
-    "database": "la-vie-migration",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USER_TEST,
+    "password": process.env.DB_PASSWORD_TEST,
+    "database": process.env.DB_NAME_TEST,
+    "host": process.env.BD_DIALECT_TEST,
+    "dialect": process.env.DB_HOST_TEST,
   },
   "production": {
     "username": "root",
